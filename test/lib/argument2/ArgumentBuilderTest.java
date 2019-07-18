6584a57cd730861eb0faf93934027b17f8758c86
Patrick Plieschnegger <patrick.plieschnegger@massiveart.com>
2019-07-18 21:38:02 +0200

    refactored test to be more meaningful

--- test/lib/argument2/ArgumentBuilderTest.java
+++ test/lib/argument2/ArgumentBuilderTest.java
@@ -11,6 +11,9 @@ import static junit.framework.TestCase.fail;
 
 public class ArgumentBuilderTest {
 
+    /**
+     * Checks for thrown exceptions for different combinations of illegal characters.
+     */
     @Test
     public void testInvalidNames() {
         List<String> invalids = Arrays.asList("-invalid", " ", "", null, "space ", "dot.", "\t", "question?");
@@ -25,34 +28,48 @@ public class ArgumentBuilderTest {
         }
     }
 
+    /**
+     * Adds argument names to the builder and checks whether they can be correctly accessed as Arguments.
+     */
     @Test
     public void testValidNames() {
-        List<String> valids = Arrays.asList("123", "alphabetical", "letter123");
+        List<String> valids = Arrays.asList("123", "alphabetical", "letter123", "uiw7e9rh2", "äöü");
+
+        var builders = List.of(new ArgumentBuilder(), new ArgumentBuilder(), new ArgumentBuilder());
+        var kinds = List.of("Option", "Argument", "Array Argument");
+
+        List<BiPredicate<Arguments, String>> predicates = List.of(
+            Arguments::hasOption,
+            Arguments::containsArgument,
+            Arguments::containsArrayArgument
+        );
 
         for (String valid : valids) {
-            var builders = List.of(new ArgumentBuilder(), new ArgumentBuilder(), new ArgumentBuilder());
-            var kinds = List.of("Option", "Argument", "Array Argument");
-
-            List<BiPredicate<Arguments, String>> predicates = List.of(
-                Arguments::hasOption,
-                Arguments::containsArgument,
-                Arguments::containsArrayArgument
-            );
-
             builders.get(0).addOption(valid);
             builders.get(1).addArgument(valid, ArgumentType.OPTIONAL);
             builders.get(2).addArrayArgument(valid, ArgumentType.OPTIONAL);
+        }
 
-            int i = 0;
-            for (var builder : builders) {
+        int i = 0;
+        for (var builder : builders) {
+            for (String valid : valids) {
                 var arguments = builder.parse(valid);
                 boolean isValid = predicates.get(i).test(arguments, valid);
 
                 String kind = kinds.get(i);
                 assertTrue(kind + " could not be defined for: " + valid, isValid);
-                ++i;
             }
-
+            ++i;
         }
     }
+
+    /*
+     * To Test
+     *  - Description
+     *  - Duplicate Entries
+     *  - Default Values
+     *  - Parsing with multiple values
+     */
+
+
 }
